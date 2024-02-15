6865292257240093133bf4722c203bdb8c970acb
Eric Eastwood <contact@ericeastwood.com>
2024-02-15 08:21:24 -0600

    Try get color to work

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
+```sh
+node generate-pass-entry-json-from-chrome-csv.js --chrome-csv test/dummy-chrome-passwords.csv
+```
+
+You can also provide `--login-alias-json` file to preload the list of suggested aliases
+to resolve conflicts.
+
+```sh
+node generate-pass-entry-json-from-chrome-csv.js --chrome-csv test/dummy-chrome-passwords.csv --login-alias-json login-aliases.json
+```
+
+```
+npm run generate-pass-entry-json-from-chrome-csv --silent -- --chrome-csv test/dummy-chrome-passwords.csv --login-alias-json login-aliases.json
+```
+
+## FAQ
+
+### Why two separate commands?
+
+So you can first generate the `chrome-pass-entries.json` file and manually review it before
+adding it to your password store.
+
 ## Piping/composing with other commands
 
-```
-FORCE_COLOR=1 npm start --silent -- --chrome-csv test/dummy-chrome-passwords.csv --login-alias-json login-aliases.json | jq .
+```sh
+npm start --silent -- --chrome-csv test/dummy-chrome-passwords.csv --login-alias-json login-aliases.json | jq .
 ```
 
-- `--silent` is necessary to suppress the `npm` output to `stdout` which messes with
-  data we're trying to pipe
-- `FORCE_COLOR=1` is necessary when piping in order to preserve color output (this
-  option comes from
-  [`chalk`](https://github.com/chalk/chalk/tree/v4.1.2?tab=readme-ov-file#chalksupportscolor))
+- `--silent` is necessary to suppress the `npm` run command output to `stdout` which
+  messes with data we're trying to pipe
+
+By default, we always enable color text output (even when piping). If you want to
+disable it, set the `FORCE_COLOR=0` environment variable (this option comes from
+[`chalk`](https://github.com/chalk/chalk/tree/v4.1.2?tab=readme-ov-file#chalksupportscolor)).
--- generate-pass-entry-json-from-chrome-csv.js
+++ generate-pass-entry-json-from-chrome-csv.js
@@ -3,12 +3,26 @@
 const assert = require('node:assert');
 const path = require('node:path');
 const { parseArgs } = require('node:util');
+const chalk = require('chalk');
 const { jsonReplacer } = require('./lib/json-serialization.js');
 const {
   parsePasswordCsvFromChrome,
   interactiveResolvePathConflictsInPassEntryMap,
   printLoginAliasMap,
 } = require('./lib/import-from-chrome.js');
+
+// Force enable color support in the terminal regardless of someone piping the output
+// (when piping, the `fd` (file descriptor) won't be a TTY which determines color
+// support by default but we want to force color). We could alternatively have users set
+// FORCE_COLOR=1 in their environment but I'd rather just have this work out of the box
+// since piping is kind of an expected use case. See
+// https://github.com/chalk/chalk/tree/v4.1.2#chalksupportscolor
+//
+// TODO: We could use this but this doesn't seem to work when you use `node
+// generate-pass-entry-json-from-chrome-csv.js ... | jq .` but does work with `npm run
+// generate-pass-entry-json-from-chrome-csv --silent -- ... | jq .`
+//
+chalk.supportsColor = true;
 
 const { values: argValues } = parseArgs({
   strict: true,
@@ -33,7 +47,7 @@ if (argValues.help) {
   process.exit(0);
 }
 
-async function importFromChromeToPass() {
+async function generatePassEntryJsonFromChromeCsv() {
   assert(argValues['chrome-csv'], 'Missing required --chrome-csv argument');
   const { baseHostToPassEntryMap } = await parsePasswordCsvFromChrome(argValues['chrome-csv']);
 
@@ -49,9 +63,11 @@ async function importFromChromeToPass() {
     baseHostToPassEntryMap,
     loginAliasMap,
   );
-  // Print to STDOUT so the user can pipe and compose this with other commands
+  // Print to stdout so the user can pipe and compose this with other commands
   console.log(JSON.stringify(pathToPassEntryMap, jsonReplacer, 2));
+  // We print the updated login alias map to stderr so it doesn't mess with our actual
+  // data output but the user can still see and copy it if they want
   printLoginAliasMap(loginAliasMap);
 }
 
-importFromChromeToPass();
+generatePassEntryJsonFromChromeCsv();
